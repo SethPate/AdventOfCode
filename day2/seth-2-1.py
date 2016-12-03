7f99dd46a0aaca357aa2803e3267d7ff35a9f385
SethPate <seth.pate@gmail.com>
2016-12-03 01:48:24 -0500

    day 2 complete

--- day2/seth-2-1.py
+++ day2/seth-2-1.py
@@ -1,4 +1,4 @@
-field = [(1, 2, 3), (4, 5, 6), (7, 8, 9)] #defines keypad
+field = ['00100', '02340', '56789', '0ABC0', '00D00'] #defines keypad
 
 input_file = 'input.txt'
 
@@ -8,6 +8,8 @@ content = input1.read()
 
 content = content.split('\n')
 
+#content = ('ULL', 'RRDDD', 'LURDL', 'UUUUD')
+
 print content
 
 code = [] #what we need to enter
@@ -15,25 +17,33 @@ code = [] #what we need to enter
 for i in field: #this shows the keypad
 	print i
 
-x = 1
-y = 1
+x = 0
+y = 2
 
 print 'starting at', (field[y][x]) # shows starting location
 
 for i in content:
 	print 'series is', i, 'starting at', field[y][x]
 	for j in i:
-		if j == 'U' and y != 0:
-			y = y - 1
-			print 'moving up to', field[y][x]
-		if j == 'R' and x != 2:
-			x = x + 1
-			print 'moving right to', field[y][x]
-		if j == 'D' and y != 2:
-			y = y + 1
-			print 'moving down to', field[y][x]
-		elif j == 'L' and x != 0:
-			x = x - 1
-			print 'moving left to', field[y][x]
+		if j == 'U':
+			if y - 1 > -1:
+				if str(field[y - 1][x]) != '0':
+					y = y - 1
+					print 'moving up to', field[y][x]
+		if j == 'R':
+			if x < 4:
+				if str(field[y][x + 1]) != '0':
+					x = x + 1
+					print 'moving right to', field[y][x]
+		if j == 'D':
+			if y < 4:
+				if str(field[y + 1][x]) != '0':
+					y = y + 1
+					print 'moving down to', field[y][x]
+		if j == 'L':
+			if x - 1 > -1:
+				if str(field[y][x - 1]) != '0':
+					x = x - 1
+					print 'moving left to', field[y][x]
 	code.append(field[y][x])
 	print code 
