649f5092bec6809eb52d025ac4ed137c6d901a0a
SethPate <seth.pate@gmail.com>
2016-12-06 18:04:43 -0500

    solved 6b.py

--- day6/seth6a.py
+++ day6/seth6a.py
@@ -27,7 +27,8 @@ for i in columns_list: #this sorts the strings by frequency and adds the most co
 	histogram = {}
 	for j in range(0,len(i)):
 		histogram[i[j]] = i.count(i[j])
-	histo_sorted = [value[0] for value in sorted(histogram.items(), key = lambda (key,value): (-value,key))]
+	histo_sorted = [value[0] for value in sorted(histogram.items(), key = lambda (key,value): (value,key))]
+#change the above to sort by (-value,key) in order to solve 6a; the above solves for 6b
 	plaintext += histo_sorted[0]
 
 print plaintext
