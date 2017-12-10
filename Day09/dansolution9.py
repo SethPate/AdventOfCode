ba778c2048b77e7cc29fa63441b2011e0938a102
Dan <dscotto10@gmail.com>
2017-12-10 14:54:19 -0800

    Dan's Day 9. Help from Seth on 9a.

--- Day09/dansolution9.py
+++ Day09/dansolution9.py
@@ -28,6 +28,32 @@ def garbfix(a):
             b = d + 1
     return c
 
+def garbcount(a):
+    e = 0
+    b = 0
+    c = ''
+    while b < len(a):
+        if a[b] != '<':
+            c = c + a[b]
+            b += 1
+        else:
+            d = a.find('>',b)
+            e += (d - b - 1)
+            b = d + 1
+    return e
+
+#stole this from Seth. Very neat!
+def score(s):
+    total = 0
+    worth = 0
+    for i in s:
+        if i == '{':
+            worth += 1
+        else:
+            total += worth
+            worth -= 1
+    return total
+
 input_text = 'daninput.txt'
 
 trashtext = fileread(input_text)
@@ -38,7 +64,7 @@ i = 0
 #get to just brackets.
 exclamcleantext = exclamfix(trashtext)
 garbcleantext = garbfix(exclamcleantext)
-#garbcleantext = garbcleantext.replace(',','')
+garbcleantext = garbcleantext.replace(',','')
 garbcleantext = garbcleantext.replace('\n','')
 
 garbcleanlist = list(garbcleantext)
@@ -47,49 +73,19 @@ garbcleanlist = list(garbcleantext)
 openbracket = 0
 closedbracket = 0
 
-for i in range(0,len(garbcleanlist)):
-    if garbcleanlist[i] == '{':
-        openbracket += 1
-    if garbcleanlist[i] == '}':
-        closedbracket += 1
+#for i in range(0,len(garbcleanlist)):
+    #if garbcleanlist[i] == '{':
+        #openbracket += 1
+    #if garbcleanlist[i] == '}':
+        #closedbracket += 1
 
-print openbracket
-print closedbracket
+#print openbracket
+#print closedbracket
 
-garbcleanlist2 = []
+#garbcleanlist2 = []
 
-for i in range(0,len(garbcleantext)):
-    if garbcleantext[i] == '{':
-        openbracket += 1
-    if garbcleantext[i] == '}':
-        closedbracket += 1
-    if openbracket == closedbracket:
-        j = i - (openbracket + closedbracket) + 1
-        garbcleanlist2.append(garbcleantext[j:i+1])
-        openbracket = 0
-        closedbracket = 0
+print "9a: " + str(score(garbcleantext))
 
-print garbcleantext
-#backwards scoring. hmm.
-#score = 0
-#scorelist = []
-#while len(garbcleantext2) > 1:
-    #for i in range(0,len(garbcleantext2)):
-        #if garbcleantext2[i:i+2] == '{}':
-            #score += 1
-    #garbcleantext2 = garbcleantext2.replace('{}','')
-    #scorelist.append(score)
-    #score = 0
+#9b solution
 
-#print scorelist
-
-#j = len(scorelist)
-#score = 0
-#for i in range(0,len(scorelist)):
-    #score += scorelist[i] * j
-    #j -= 1
-
-
-
-#print scorelist
-#print score
+print "9b: " + str(garbcount(exclamcleantext))
