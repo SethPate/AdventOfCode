980e2341e64f57dfc100f160c6a4f78f3763a8c7
SethWPate <seth.pate@gmail.com>
2017-12-11 16:29:50 -0500

    edited 10b
    still not working; posting for dan

--- Day10/sethsolution10_clean.py
+++ Day10/sethsolution10_clean.py
@@ -15,19 +15,14 @@ def stringSwitcher(l, position, length):
     
     return newList
 
-def knot(s,datarange,iterations):
-    """input: a string of digit instructions separated by ',',
+def knot(lengths,datarange,iterations):
+    """input: a list of integers lengths,
     a range of data, and # of iterations
     output: a list of digits that has been "knotted"
     """
-    assert type(s) == str
+    assert type(lengths) == list
     assert type(datarange) == int
     assert type(iterations) == int
-    
-    lengths = [] #this is my instructions
-    s = s.split(',') #split the string on a comma
-    for i in s:
-        lengths.append(int(i)) #add the integer interpretation to lengths
         
     data = [i for i in range(datarange)] #create the plaintext
 
@@ -42,9 +37,75 @@ def knot(s,datarange,iterations):
     
     return data
 
+def getLengths(s):
+    """input: takes a string of any characters as instructions
+    output: the ASCII representation of these characters, plus arbitrary suffix
+    """
+    assert type(s) == str
+    lengths = []
+    lengths.extend(ord(character) for character in s)
+    while 10 in lengths:
+        lengths.remove(10) #remove any there's a pesky \n character at the end of s
+    lengths.extend([17, 31, 73, 47, 23])
+    return lengths
+
+def xorMaker(l):
+    """input: a list of digits divisible by 16
+    output: a list of digits, each digit is the result of 16 digits in serial XOR
+    """
+    assert len(l) % 16 == 0
+    
+    denseHash = []
+    copy = l.copy()
+    
+    while len(copy) > 0:
+        block = copy[:16]
+        xorResult = 0
+        for i in block:
+            xorResult ^= i
+        denseHash.append(xorResult)
+        del copy[:16]
+    
+    return denseHash
+
+def getHex(l):
+    """input: a list of digits
+    output: a string of the hex representation of input digits
+    """
+    hexString = ''
+    for digit in l:
+        hexString += hex(digit)[2:]
+    return hexString
+
+f = open('sethinput.txt', 'r')
+reader = f.read()
+reader = reader.split(',') #split the string on a comma
+lengths = [] #this is my instructions
+for i in reader:
+    lengths.append(int(i)) #add the integer interpretation to lengths
+
+answerA = knot(lengths,256,1)
+
+print('answer to part a is', answerA[0] * answerA[1])
+
+"""part B"""
+
 f = open('sethinput.txt', 'r')
 reader = f.read()
 
-answerA = knot(reader,256,1)
+#convert instruction string to ASCII and add suffix
 
-print('answer to part a is', answerA[0] * answerA[1])
+lengths = getLengths(reader)
+
+#run 64 loops
+
+sparseHash = knot(lengths,256,64)
+
+#convert blocks of 16 characters, using serial XOR, for a list of 16 characters
+
+denseHash = xorMaker(sparseHash)
+
+#convert digits to hex characters, for 32 character string
+
+hexString = getHex(denseHash)
+    
