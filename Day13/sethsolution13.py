09479e19a628754e9d9d9c1c555355294cfcdbfe
SethWPate <seth.pate@gmail.com>
2017-12-13 14:52:02 -0500

    stuck on 13b

--- Day13/sethsolution13.py
+++ Day13/sethsolution13.py
@@ -1,3 +1,5 @@
+import copy
+
 def inputParser(string):
     #takes a string of input and turns it into a useful list of digits
     string = string.strip()
@@ -20,14 +22,14 @@ def firewallFinisher(dictionary):
     checker = set([i for i in range(max(firewall.keys()))])
     checkAgainst = set(firewall.keys())
     for i in checker.difference(checkAgainst):
-        print(i)
+#        print(i)
         dictionary[i] = 'skip'
-        print(dictionary[i])
-    print(dictionary)
+#        print(dictionary[i])
+#    print(dictionary)
     return dictionary
 
 def wallUpdater(dictionary):
-    #updates the firewall after the scanners move
+    #updates the firewall after the scanners move, returns the new dict
     for layer in dictionary:
         if not dictionary[layer] == 'skip':
             position = dictionary[layer][0].index(1) #mark where the scanner is
@@ -40,18 +42,29 @@ def wallUpdater(dictionary):
                 dictionary[layer][0][position - 1] = 1
                 if dictionary[layer][0][0] == 1:
                     dictionary[layer][1] = True
+    return dictionary
 
-def partA(dictionary):
+def severity(dictionary):
     #given a firewall, returns the "severity" of attempting to cross
-    #has to remind wallUpdater of which direction to update
+    hypotheticalDictionary = copy.deepcopy(dictionary)
     severity = 0
-    for i in range(len(dictionary)): #iterate for as many layers as there are in the firewall
-        print('now at location', i)
-        if dictionary[i][0][0] == 1:
-            print('encountered security, adding', i * len(dictionary[i][0]))
-            severity += i * len(dictionary[i][0])
-        wallUpdater(dictionary)
+    for i in range(len(hypotheticalDictionary)): #iterate for as many layers as there are in the firewall
+#        print('now at location', i, 'layer is', hypotheticalDictionary[i][0])
+        if hypotheticalDictionary[i][0][0] == 1:
+#            print('encountered security, adding', i * len(hypotheticalDictionary[i][0]))
+            severity += i * len(hypotheticalDictionary[i][0])
+        hypotheticalDictionary = wallUpdater(hypotheticalDictionary)
     return severity
+
+def isCaught(dictionary):
+    #true if trying to get through the firewall would get you caught
+    dictCopy = copy.deepcopy(dictionary)
+    for i in range(len(dictCopy)):
+        if dictCopy[i][0][0] == 1:
+#            print('caught by security at layer', i, 'see', dictCopy[i][0])
+            return True
+        dictCopy = wallUpdater(dictCopy)
+    return False
 
 f = open('sethinput.txt', 'r')
 data = f.readlines()
@@ -63,4 +76,15 @@ for instruction in data:
 
 firewallFinisher(firewall)
 
-print('answer to part A is', partA(firewall))
+print('answer to part A is', severity(firewall))
+
+delay = 0
+
+while isCaught(firewall):
+    delay += 1
+    firewall = wallUpdater(firewall)
+#    print('delay of', delay, 'isCaught?', isCaught(firewall))
+    if delay > 100000:
+        break
+
+print('part B answer is', delay)
