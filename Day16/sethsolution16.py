f1024e74d01d5adc7245578eac64eda35d55198e
SethWPate <seth.pate@gmail.com>
2017-12-16 13:08:03 -0500

    more help from dan

--- Day16/sethsolution16.py
+++ Day16/sethsolution16.py
@@ -3,7 +3,7 @@ class linedance(str):
         self.dancers = dancers
         
     def spin(self, x):
-        #moves x digits from the end of the string to the beginning, returns a modified str
+        #moves x digits from the end of the string to the beginning, updates the dancers to a modified str
         newString = ''
         movingPart = self.dancers[-x:]
         newString += movingPart
@@ -11,29 +11,31 @@ class linedance(str):
         self.dancers = newString
     
     def exchange(self, a, b):
-        #switches the dancers at positions a and b, returns a modified str
+        #switches the dancers at positions a and b, updates the dancers to a modified str
         locations = [a,b] #these are the places that are switching
         locations.sort() #have to get them in the right order
-        print('locations', locations)
         newString = ''
         x = locations[0] #location of first dancer
-        print('x', x)
         y = locations[1] #location of second dancer
-        print('y', y)
         dancerX = self.dancers[x] #save who's in the first spot
-        print('dancerX', dancerX)
         dancerY = self.dancers[y] #save who's in the second spot
-        print('dancerY', dancerY)
         newString += self.dancers[:x] #add anything before the first location
-        print(newString)
         newString += dancerY #then add who was in the second index before
-        print(newString)
         newString += self.dancers[x+1:y] #add anything between the two locations
-        print(newString)
         newString += dancerX #add who was in the first index
-        print(newString)
         newString += self.dancers[y+1:]
-        return newString
+        self.dancers = newString
         
+    def partner(self, a, b):
+        #switches the location of dancers a and b, updates the dancers to a modified str
+        newString = ''
+        x = self.dancers.index(a) #find the index of dancer a
+        y = self.dancers.index(b) #and the index of dancer b
+        newString = self.exchange(x,y)
+#        locations = [x,y]
+#        locations.sort()
+#        x = locations[0] #location of first dancer
+#        y = locations[1] #location of second dancer
+        self = newString
 
 partA = linedance('abcde')
