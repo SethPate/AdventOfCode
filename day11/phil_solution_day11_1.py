66abd21bfb3b13c7acdd4a061f60fe5eb22ac2e8
pates <pates@husky.neu.edu>
2018-11-28 10:58:32 -0500

    consolidating advent of code challenges.

--- day11/phil_solution_day11_1.py
+++
@@ -1,8 +0,0 @@
-from floor import *
-
-floors = {1: Floor(1), 2: Floor(2), 3: Floor(3), 4: Floor(4)}
-
-floors[1].addGenerator('pro').addChip('pro')
-floors[2].addGenerator('cob').addGenerator('cur').addGenerator('rut').addGenerator('plu')
-floors[3].addChip('cob').addChip('cur').addChip('rut').addChip('plu')
-
