bb0d82c25ed71cb628684fdde5762284807f7749
agduff <agduff@gmail.com>
2018-12-26 18:17:10 -0500

    input as downloaded...

---
+++ 2018/Day04/agd_4-1.py
@@ -0,0 +1,19 @@
+datum = []
+cloth = []
+dup_cloth = []
+with open("agd_input4.txt", "r") as x:
+    data = x.readlines()
+
+for line in data:
+    datum.append(line.split())
+
+for x in range(len(data)):
+    datum[x].append(datum[x][1].split(":")[0])
+    datum[x].append(datum[x][1].split(":")[1].split("]")[0])
+
+print(datum[43])
+print(datum[22])
+print(datum[431])
+print(datum[366])
+print(datum[127])
+print(datum[4])
