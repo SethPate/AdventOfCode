ace0d06ebcb7d08437ce1692e485e049a68b0dea
RHPeel <87251403+RHPeel@users.noreply.github.com>
2021-12-07 15:07:01 -0500

    Update DCS_Day7.py
    I refactored a bit as an exercise.

--- 07/DCS_Day7.py
+++ 07/DCS_Day7.py
@@ -13,56 +13,47 @@ def listmaker(a):
         c.sort()
     return c
 
-def get_distance_7a(a,b):
-    return abs(a - b)
-
-def get_distance_7b(a,b):
-    c = 0
-    d = abs(b - a)
-    if d % 2 == 0:
-        c = (d**2) / 2 + (0.5 * d)
-    elif d % 2 == 1:
-        c = ((d + 1)**2) / 2 + (0.5 * (d + 1)) - (d + 1)
-    return c
+def get_distance(a,b,c):
+    if c == '7A':
+        return abs(a - b)
+    elif c == '7B':
+        c = 0
+        d = abs(b - a)
+        if d % 2 == 0:
+            c = (d**2) / 2 + (0.5 * d)
+        elif d % 2 == 1:
+            c = ((d + 1)**2) / 2 + (0.5 * (d + 1)) - (d + 1)
+        return c
 
 class Crabs:
-    def __init__(self,a):
+    def __init__(self,a,b):
         self.start_list = a
         self.min = a[0]
         self.max = a[-1]
-        self.count = len(a)
-        self.range = self.max - self.min
-        self.distance_7b = 0
         self.nav_dict = {}
+        self.puzzle = b
+
+    def find_fuel_usage(self):
+        for i in range(0,self.max + 1):
+            self.nav_dict[i] = 0
+            for item in self.start_list:
+                self.nav_dict[i] += get_distance(i,item,self.puzzle)
+                
+    def get_min_fuel(self):
+        return min(self.nav_dict.values())
 
 
 data_input = fileimport('day7_input.txt')
 input_list = listmaker(data_input)
 
-crab_set = Crabs(input_list)
-
 #7A
 
-position_dict = {}
-
-for i in range(0,crab_set.max + 1):
-    position_dict[i] = 0
-    for item in crab_set.start_list:
-        position_dict[i] += get_distance_7a(item,i)
-
-min_value = min(position_dict.values())
-
-print("7A: " + str(min_value))
+crab_set_7a = Crabs(input_list,'7A')
+crab_set_7a.find_fuel_usage()
+print("7A: " + str(crab_set_7a.get_min_fuel()))
 
 #7B
 
-position_dict_7b = {}
-
-for i in range(0,crab_set.max + 1):
-    position_dict_7b[i] = 0
-    for item in crab_set.start_list:
-        position_dict_7b[i] += get_distance_7b(i,item)
-
-min_value_7b = min(position_dict_7b.values())
-
-print("7B: " + str(min_value_7b))
+crab_set_7b = Crabs(input_list,'7B')
+crab_set_7b.find_fuel_usage()
+print("7B: " + str(crab_set_7b.get_min_fuel()))
